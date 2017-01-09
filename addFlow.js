09766cd3e739846cb9ad18423d5fd1930ddffe30
Pitarn Harnpanichpun <pitarnh@outlook.com>
2017-01-09 14:11:00 +0700

    change request method and update gitignore

--- addFlow.js
+++ addFlow.js
@@ -15,7 +15,7 @@ var request=require('request');
 var putData1={
     'installInHw':'true',
     'name':'s1-flow1',
-    'node':{'id':'11:11:11:11:11:11:11:01','type':'OF'},
+    'node':{'id':'00:00:00:00:00:00:00:01','type':'OF'},
 	  'ingressPort':'1',
 	  'actions':['OUTPUT=2']
 };
@@ -32,7 +32,7 @@ var putData1={
 var putData2={
     'installInHw':'true',
     'name':'s1-flow2',
-    'node':{'id':'11:11:11:11:11:11:11:01','type':'OF'},
+    'node':{'id':'00:00:00:00:00:00:00:01','type':'OF'},
 	  'ingressPort':'2',
 	  'actions':['OUTPUT=1']
 
@@ -43,9 +43,8 @@ var putData2={
 var options1={
   url:'localhost:8181/controller/nb/v2/switchmanager/default/node/OF/00:00:00:00:00:00:00:01/property/description/s1',
   headers:{'Content-type':'application/json','Accept':'application/json'},
-  username:'admin',
-  password:'admin'
-
+  // username:'admin',
+  // password:'admin'
 };
 //------------ options for add flow ------------
 var options2={
@@ -57,10 +56,13 @@ var options2={
 var options3={
   url:'localhost:8181//controller/nb/v2/flowprogrammer/default/node/OF/00:00:00:00:00:00:00:01/staticFlow/s1_2',
   headers:{'Content-type':'application/json','Accept':'application/json'},
-  username:'admin',
-  password:'admin'
+  // username:'admin',
+  // password:'admin'
 }
 //------------ perform http req for add name ------------
+var username='admin';
+var pass='admin';
+request.get('localhost:8181/index.html').auth(username,pass);
 request.post(options1,function(err,httpResponse,body){
   if(httpResponse){
     console.log(res.statusCode);
@@ -71,6 +73,7 @@ request.post(options1,function(err,httpResponse,body){
   console.log(body)
 });
 //------------ perform http req for add flow ------------
+request.get('localhost:8181/index.html').auth(username,pass);
 request.post(options2,putData1,function(err,httpResponse,body){
   if(httpResponse){
     console.log(res.statusCode);
